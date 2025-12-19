9f8bf7440798d0b65626f2dd04a7de89fd967c0f
jjforsyth15 <jjforsyth15@gmail.com>
2025-12-19 01:54:07 -0800

    Initialized Setup

---
+++ src/app/auth/auth-service.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-service',
+  imports: [],
+  templateUrl: 'auth-service.html',
+  styleUrl: 'auth-service.css',
+})
+export class AuthService {
+
+}
---
+++ src/app/restaurant/restaurant.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant',
+  imports: [],
+  templateUrl: './restaurant.html',
+  styleUrl: './restaurant.css',
+})
+export class Restaurant {
+
+}
